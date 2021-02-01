76f53ba575f6b660c4442301b641fd1054b547a2
ban-mazin <ban.qaddoori89@gmail.com>
2021-02-01 06:44:14 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,23 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+
+//ex2
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//ex3
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//ex4
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
